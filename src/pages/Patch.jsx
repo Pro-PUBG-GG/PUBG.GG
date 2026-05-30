8a114017ed6a7de6a349c6d5f1944c1447f2211e
ygunk <ygunkun1015@gmail.com>
2026-05-30 22:43:20 +0900

    feat: standardize page header layout

--- src/pages/Patch.jsx
+++ src/pages/Patch.jsx
@@ -1,7 +1,25 @@
+//packages & components
+import styled from "styled-components";
+import { GlobalStyle } from "./Main";
+import Header from "../component/Header";
+//assets
+
+//styled-components
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: #d9d9d9;
+  margin: 0;
+  padding: 0;
+`;
+
 export default function PatchPage(){
     return(
         <>
-        
+            <GlobalStyle />
+            <Container>
+                <Header name="patch" />
+            </Container>
         </>
     )
 }
--- src/pages/Simulation.jsx
+++ src/pages/Simulation.jsx
@@ -1,7 +1,26 @@
+//packages & components
+import styled from "styled-components"
+import { GlobalStyle } from "./Main"
+import Header from "../component/Header"
+//assets
+import logo from "../assets/logo.svg"
+import back_image from "../assets/Simulation.png"
+//styled-components
+const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: url(${back_image}) center/cover no-repeat;
+  margin: 0;
+  padding: 0;
+`;
+
 export default function SimulationPage(){
     return(
         <>
-        
+            <GlobalStyle />
+            <Container>
+                <Header name="sim" />
+            </Container>
         </>
     )
 }
